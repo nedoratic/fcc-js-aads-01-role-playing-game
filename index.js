c713ee0728e88d5e68f84404b5827d7407d365b7
nedoratic <nedoratic@gmail.com>
2024-01-22 19:02:24 +0100

    goStore function edited

--- index.js
+++ index.js
@@ -17,8 +17,19 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+// initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
 function goStore() {
-	console.log("Going to store.");
+	button1.innerText = "Buy 10 health (10 gold)";
+	button2.innerText = "Buy weapon (30 gold)";
+	button3.innerText = "Go to town square";
+	button1.onclick = buyHealth;
+	button2.onclick = buyWeapon;
+	button3.onclick = goTown;
+	text.innerText = "You enter the store.";
 }
 
 function goCave() {
@@ -28,8 +39,3 @@ function goCave() {
 function fightDragon() {
 	console.log("Fighting dragon.");
 }
-
-// initialize buttons
-button1.onclick = goStore;
-button2.onclick = goCave;
-button3.onclick = fightDragon;
