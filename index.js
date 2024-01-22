fb3238ed7d75a5df657c52faa43c58250f512fed
nedoratic <nedoratic@gmail.com>
2024-01-22 18:50:59 +0100

    buttons initialized

--- index.js
+++ index.js
@@ -28,3 +28,8 @@ function goCave() {
 function fightDragon() {
 	console.log("Fighting dragon.");
 }
+
+// initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
