061e4b84dc46b63de8aa2d34e88d285b44488350
nedoratic <nedoratic@gmail.com>
2024-01-23 11:04:15 +0100

    goTown and goStore functions updated to use the call the update function with adequate arguments

--- index.js
+++ index.js
@@ -52,32 +52,16 @@ function update(location) {
 
 // Functions
 function goTown() {
-	button1.innerText = "Go to store";
-	button2.innerText = "Go to cave";
-	button3.innerText = "Fight dragon";
-	button1.onclick = goStore;
-	button2.onclick = goCave;
-	button3.onclick = fightDragon;
-	text.innerText = 'You are in the town square. You see a sign that says "Store".';
+	update(locations[0]);
 }
 
 function goStore() {
-	button1.innerText = "Buy 10 health (10 gold)";
-	button2.innerText = "Buy weapon (30 gold)";
-	button3.innerText = "Go to town square";
-	button1.onclick = buyHealth;
-	button2.onclick = buyWeapon;
-	button3.onclick = goTown;
-	text.innerText = "You enter the store.";
+	update(locations[1]);
 }
 
-function goCave() {
-	console.log("Going to cave.");
-}
+function goCave() {}
 
-function fightDragon() {
-	console.log("Fighting dragon.");
-}
+function fightDragon() {}
 
 function buyHealth() {}
 
