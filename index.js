ea9a70a409717844814718b9f2db223b20409392
nedoratic <nedoratic@gmail.com>
2024-01-28 21:34:26 +0100

    getMonsterAttackValue function added

--- index.js
+++ index.js
@@ -184,10 +184,15 @@ function goFight() {
 	monsterHealthText.innerText = monsterHealth;
 }
 
+function getMonsterAttackValue(level) {
+	const hit = level * 5 - Math.floor(Math.random() * xp);
+	return hit;
+}
+
 function attack() {
 	text.innerText = "The " + monsters[fighting].name + " attacks.";
 	text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
-	health -= monsters[fighting].level;
+	health -= getMonsterAttackValue(monsters[fighting].level);
 	monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
 	healthText.innerText = health;
 	monsterHealthText.innerText = monsterHealth;
